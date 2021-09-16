523ab4a8f8d4134de6ba8a8bc2805be779ac0345
ostertun <dev@ostertun.net>
2021-09-16 12:51:21 +0200

    track song before skip

--- api/api/skip.php
+++ api/api/skip.php
@@ -1,13 +1,24 @@
 <?php
-	
+
 	/* Sends an SKIP request to the currently playing device
 	 *
 	 * Required REQUEST params:
 	 *  ---
 	 */
-	
+
 	require_once(__DIR__ . '/../access.php');
-	
+
+	// *** GET CURRENT SONG ***
+	$track = spotify_getsong();
+
 	spotify_next();
-	
+
+	if (($track !== false) and (isset($track['item']))) {
+		$trackid = $track['item']['id'];
+		// *** REMOVE SONG FROM PL_WISH ***
+		spotify_remove(SP_USERNAME, PL_WISH, $trackid);
+		// *** REMOVE SONG FROM PL_POOL ***
+		spotify_remove(SP_USERNAME, PL_POOL, $trackid);
+	}
+
 ?>
